85fd45a6d069cc7273f992c85d62a014d6b83d24
Arnold Murphy <user.email>
2025-04-02 13:10:46 -0600

    c-20

--- Iterations/while_loop.py
+++ Iterations/while_loop.py
@@ -6,6 +6,25 @@ This module provides basic collections dictionary operations.
 i = 0
 print(i)
 
+#iterator
+while i < 5:
+    print(i)
+    i += 1
+
+# List of fruits
+fruits= [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',     
+]
+
+print("Our Fruit Selection")
+# Loop through the list of spices
+for fruit in fruits:
+    print(fruit)
 
 
 #End-of-file (EOF)(\n)
