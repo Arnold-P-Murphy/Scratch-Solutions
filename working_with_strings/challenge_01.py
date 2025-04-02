6ee5e6a4bdbdd4a604804eb4e3b210312482b5fe
Arnold Murphy <user.email>
2025-04-02 14:59:45 -0600

    c-45

--- working_with_strings/challenge_01.py
+++ working_with_strings/challenge_01.py
@@ -18,6 +18,10 @@ kilometers = float(miles) * 1.60934 # 1.60934
 print(miles + " miles is equal to " + str(kilometers) + " kilometers.")
 # Print the result
 
+kilometers = input("Enter the number of kilometers: ")
+# Convert kilometers to miles
+print(kilometers + " kilometers is equal to " + str(float(kilometers) / 1.60934) + " miles.")
+# Convert kilometers to miles
 
 
 # End-of-file (EOF)
