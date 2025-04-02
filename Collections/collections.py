41c1b404164a804299dbee4f7faad35dacd3c701
Arnold Murphy <user.email>
2025-04-02 10:41:56 -0600

    COMMIT12 FIX PROBLEMS

--- Collections/collections.py
+++ Collections/collections.py
@@ -1,14 +1,19 @@
+"""
+This module provides basic collections dictionary operations.
+"""
 # Nearest stars to Earth
-star1 = 'Sol'
-star2 = 'Alpha Centauri'
-star3 = 'Barnard'
-star4 = 'Wolf 359'
+STAR_1 = 'Sol'
+STAR_2 = 'Alpha Centauri'
+STAR_3 = 'Barnard'
+STAR_4 = 'Wolf 359'
 
 # Highest peak on each tectonic plate
-African = 'Kilimanjaro'
-Antarctic = 'Vinson'
-Australian = 'Puncak Jaya'
-Eurasian = 'Everest'
-North_American = 'Denali'
-Pacific = 'Mauna Kea'
-South_American = 'Aconcagua'
+AFRICAN = 'Kilimanjaro'
+ANTARCTIC = 'Vinson'
+AUSTRALIAN = 'Puncak Jaya'
+EURASIAN = 'Everest'
+NORTH_AMERICAN = 'Denali'
+PACIFIC = 'Mauna Kea'
+SOUTH_AMERICAN = 'Aconcagua'
+
+# Dictionary of tectonic plates and their respective highest peaks
