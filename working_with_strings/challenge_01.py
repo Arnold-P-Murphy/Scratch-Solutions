35533d20a08c87685e823ee4baa2e5dd3f34069a
Arnold Murphy <user.email>
2025-04-02 14:57:02 -0600

    c-43

--- working_with_strings/challenge_01.py
+++ working_with_strings/challenge_01.py
@@ -5,4 +5,15 @@ Author: Arnold Murphy
 Date: 2025-04-02
 """
 
+miles = input("Enter the number of miles: ")
+# Convert miles to kilometers
+
+print(miles + " miles is equal to " + str(float(miles) * 1.60934) + " kilometers.")
+# Convert miles to kilometers
+
+
+
+
+
+
 # End-of-file (EOF)
