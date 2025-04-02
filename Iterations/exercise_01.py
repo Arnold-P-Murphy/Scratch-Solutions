a2d122d4af93e844e168579726e7c860cb6c7dfe
Arnold Murphy <user.email>
2025-04-02 13:37:58 -0600

    c*32

---
+++ Iterations/exercise_01.py
@@ -0,0 +1,25 @@
+"""
+This module demonstrates a simple iteration over a list of fruits.
+
+Author: Arnold Murphy
+Date: 2025-04-02
+"""
+
+# List of fruits
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangoes',  # Corrected spelling
+    'nectarines',
+    'pears',
+]
+
+# Print the header
+print("Our Fruit Selection")
+
+# Loop through the list of fruits and print each one
+for fruit in fruits:
+    print(fruit)
+
+# End-of-file (EOF)
