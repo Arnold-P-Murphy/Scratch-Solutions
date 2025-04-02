5ae65040f9b6cd1a03bf59cb323aef7d584110c8
Arnold Murphy <user.email>
2025-04-02 13:48:11 -0600

    c-24

---
+++ Iterations/exercise_02.py
@@ -0,0 +1,25 @@
+"""
+This module demonstrates a simple iteration over a list of fruits.
+
+Author: Arnold Murphy
+Date: 2025-04-02
+"""
+
+i = 0
+
+
+#creating lists
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+#output: [1, 2, 3, 4, 5]
+
+print(my_list[i])
+#output: 1
+
+
+
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+
+# End-of-file (EOF)
