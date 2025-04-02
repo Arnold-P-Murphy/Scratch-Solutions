0148770206d43c582be9a99068aa5e28f098b74d
Arnold Murphy <user.email>
2025-04-02 09:59:48 -0600

    commit6

--- working_with_lists.py
+++ working_with_lists.py
@@ -15,13 +15,6 @@
 my_list = [1, 2, 3, 4, 5]
 print(my_list)
 #output: [1, 2, 3, 4, 5]
-my_list = ['apple', 'banana', 'cherry']
-print(my_list)
-#output: ['apple', 'banana', 'cherry']
-my_list = [1, 'apple', 3.14, True]
-
-print(my_list)
-
 
 print(my_list[0])
 #output: 1
